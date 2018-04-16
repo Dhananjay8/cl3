eec9286b61c16fe8fdfc71f0e6411e705b40df8b
Dhananjay Patil <dhananjaypatil070@gmail.com>
2018-04-16 19:24:25 +0530

    Update d3.py
    best code ever!!!

--- d3/d3.py
+++ d3/d3.py
@@ -1,86 +1,75 @@
-import random, math
-class Dsa:
-	def __init__(self):
-		self.p=None
-		self.q=None
-		self.g=None
-		self.x=None
-		self.y=None
-		self.k=None
-		self.H=None
+from random import randint
+import hashlib
+from math import floor
 
-	def isPrime(self,a):
-		if a<=1 or a%2==0:
-			return False
-		if a==2:
-			return True
-		i=3
-		while i<=math.ceil(math.pow(a,0.5)):
-			if a%i==0:
-				return False
-			i+=2
+def isprime(n):
+	if n==1 or n==0:
+		return False
+	return all([n%i for i in range(2,int(floor(n**0.5))+1)])
+	
+def modpow(base,exp,mod):
+	ans=1
+	for i in range(exp):
+		ans=(ans*base)%mod
+	return ans
+
+def modinv(num,mod):
+	for i in range(1,mod):
+		if (num*i)%mod==1:
+			return i
+			
+def gen_p(q):
+	assert (isprime(q)), "Must be Prime!!"
+	mul=2
+	while True:
+		p=mul*q+1
+		mul+=1
+		if isprime(p):
+			return p
+	
+def gen_g(p,q):
+	for h in range(2,p-1):
+		g=modpow(base=h,exp=(p-1)/q,mod=p)
+		if g>1:
+			return g
+	raise
+	
+def gen_keys(p,q,g):
+	pr=randint(1,q-1)
+	pu=modpow(base=g,exp=pr,mod=p)
+	return pr,pu
+
+def gen_hash(text):
+	return int(hashlib.sha1(text).hexdigest(),16)
+
+def gen_signature(Hm,p,q,g,pr):
+	k=randint(2,q-1)
+	kinv=modinv(num=k,mod=q)
+	r=modpow(base=g,exp=k,mod=p)%q
+	s=(kinv*(Hm+pr*r))%q
+	return r,s
+
+def verify(Hm,r,s,p,q,g,pu):
+	w=modinv(num=s,mod=q)
+	u1=(Hm*w)%q
+	u2=(r*w)%q
+	v=modpow(base=g,exp=u1,mod=p)*modpow(base=pu,exp=u2,mod=p)
+	v=(v%p)%q
+	print 'v & r:',v,r
+	if v==r:
 		return True
+	return False	
 
-	def genG(self):
-		primeList=[]
-		for x in range(4,100):
-			if self.isPrime(x):
-				primeList.append(x)
-		self.p=random.choice(primeList)
-		print "Value of p is ",self.p
 
-		for i in range(1,self.p-1):
-			if (self.p-1)%i==0:
-				if self.isPrime(i):
-					self.q=i
-		print "Value of q is ",self.q
-
-		h=random.randint(2,self.p-1)
-		print "value of h is ",h
-
-		self.g=pow(h,(self.p-1)/self.q)%self.p
-		print "value of g is ",self.g
-
-	def genK(self):
-		self.x=random.randint(1,self.q-1)
-		self.y=pow(self.g,self.x)%self.p
-		print "Private Key, Public Key: ", self.x,self.y
-
-		self.k=random.randint(1,self.q-1)
-		print "Value of K is ",self.k
-
-	def createSign(self):  
-		self.H=int(raw_input("Enter an Integer Hash: "))
-		r=math.pow(self.g,self.k)%self.p
-		r=r%self.q
-		print "r component of signature is ",r#int(r)
-		s=((self.H+self.x*r)/self.k)%self.q
-		print "s component of signature is ",s#int(s)
-		return r,s
-
-	def calW(self,s):
-		z=self.q
-		s=s%self.q
-		for i in range(1,z+1):
-			if (s*i)%z==1%z:
-				#print "got inverse"
-				return i
-		return pow(int(s),z-2,z)
-
-	def calU1(self,r,s):
-		return (self.H*self.calW(s))%self.q
-
-	def calU2(self,r,s):
-			return (r*self.calW(s))%self.q
-
-	def verifySign(self,r,s):
-		u1=self.calU1(r,s)
-		u2=self.calU2(r,s)
-		return ((math.pow(self.g,u1)*math.pow(self.y,u2))%self.p)%self.q
-
-d=Dsa()
-d.genG()
-d.genK()
-rc,sc=d.createSign()
-v=d.verifySign(rc,sc)
-print "V is ",v
+q=int(raw_input("Enter q:"))
+p=gen_p(q)
+print "p,q :", p,q
+g=gen_g(p,q)
+print "g: ",g
+pr,pu=gen_keys(p,q,g)
+print "Private and Public Keys: ",pr,pu
+text=str(raw_input("Enter Text Message: "))
+hashh=gen_hash(text)
+print hashh
+r,s=gen_signature(hashh,p,q,g,pr)
+print verify(hashh,r,s,p,q,g,pu)
